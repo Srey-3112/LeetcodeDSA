94f158a39a64a5aecaed5534e4ab4e571ac83b6c
Srey Patel <83701089+Srey-3112@users.noreply.github.com>
2024-06-11 17:25:57 +0530

    Time: 186 ms (99.76%), Space: 0B (100%) - LeetHub

--- 1801-average-time-of-process-per-machine/1801-average-time-of-process-per-machine.sql
+++ 1801-average-time-of-process-per-machine/1801-average-time-of-process-per-machine.sql
@@ -4,5 +4,6 @@ from Activity a1
 inner join Activity a2
 on a1.machine_id = a2.machine_id
 and a1.process_id = a2.process_id
-and a1.timestamp<a2.timestamp
+and a1.activity_type="start" 
+and a2.activity_type="end"
 group by a1.machine_id
