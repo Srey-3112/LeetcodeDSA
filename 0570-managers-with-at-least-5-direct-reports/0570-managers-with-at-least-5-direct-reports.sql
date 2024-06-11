d8f2c54f3bba668b3ab44288f7163426ad063d5c
Srey Patel <83701089+Srey-3112@users.noreply.github.com>
2024-06-11 18:57:34 +0530

    Time: 456 ms (92.66%), Space: 0B (100%) - LeetHub

---
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -0,0 +1,16 @@
+# Write your MySQL query statement below+-----+-------+------------+-----------+
+-- | id  | name  | department | managerId |
+-- +-----+-------+------------+-----------+
+-- | 101 | John  | A          | null      |
+-- | 102 | Dan   | A          | 101       |
+-- | 103 | James | A          | 101       |
+-- | 104 | Amy   | A          | 101       |
+-- | 105 | Anne  | A          | 101       |
+-- | 106 | Ron   | B          | 101       |
+-- +-----+-------+------------+-----------+
+select e1.name
+from Employee e1
+inner join Employee e2
+on e1.id=e2.managerId
+group by e2.managerId
+having count(e2.managerId)>=5
