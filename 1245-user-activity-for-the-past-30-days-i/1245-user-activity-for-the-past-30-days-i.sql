404131022ad182c0dca0fb6d34532e4c0f548f89
Srey Patel <83701089+Srey-3112@users.noreply.github.com>
2024-06-13 07:51:45 +0530

    Time: 433 ms (98.11%), Space: 0B (100%) - LeetHub

--- 1245-user-activity-for-the-past-30-days-i/1245-user-activity-for-the-past-30-days-i.sql
+++ 1245-user-activity-for-the-past-30-days-i/1245-user-activity-for-the-past-30-days-i.sql
@@ -1,4 +1,10 @@
 # Write your MySQL query statement below
+-- select activity_date as day, count(distinct user_id) as active_users
+-- from Activity
+-- where activity_date >= date_sub("2019-07-27", interval 30 day)
+-- and activity_date<= "2019-07-27"
+-- group by activity_date
+# Write your MySQL query statement below
 SELECT activity_date AS day, COUNT(DISTINCT user_id) AS active_users
 FROM Activity
 WHERE activity_date BETWEEN '2019-06-28' AND '2019-07-27'
