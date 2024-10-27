c87eb0090e48075cf6cfda6cd7d80666ab4990ca
Srey Patel <83701089+Srey-3112@users.noreply.github.com>
2024-10-27 10:27:11 -0400

    Time: 0 ms (100%), Space: 9.4 MB (28.77%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,63 @@
+class Solution {
+
+private:
+    bool valid(char c){
+        if((c>='a' && c<='z')||(c>='A' && c<='Z')||(c>='0' && c<='9')){
+            return true;
+        }
+        return false;
+    }
+
+    char toLowerCase(char c){
+        if((c>='a' && c<='z')||(c>='0' && c<='9')){
+            return c;
+        }
+        else{
+            char temp= c+'a'-'A';
+            return temp;
+        }
+    }
+public:
+    bool isPalindrome(string s) {
+        string temp="";
+        for(int j=0;j<s.length();j++){
+            if(valid(s[j])){
+                temp.push_back(s[j]);
+            }
+        }
+        for(int j=0;j<temp.length();j++){
+            temp[j]= toLowerCase(temp[j]);
+        }
+        
+        int left=0;
+        int right=temp.size()-1;
+
+        while(left<=right){
+            if(temp[left]!=temp[right]){
+                return false;
+            }
+            else{
+                left++;
+                right--;
+            }
+        } 
+        return true;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+    }
+};
