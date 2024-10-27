00695ac68055a277e4ee092fca38514d968481ed
Srey Patel <83701089+Srey-3112@users.noreply.github.com>
2024-10-26 22:18:57 -0400

    Time: 3 ms (67.8%), Space: 8.8 MB (82.9%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,22 +1,37 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        
-        map<char,int> mpp;
-        if(s.size() != t.size()){
-            return false;
-        }
 
-        for(int i=0;i<s.size();i++){
-            mpp[s[i]]++;
-            mpp[t[i]]--;
-        }
-        for(auto it : mpp){
-            if(it.second !=0){
-                return false;
-            }
-        }
-        return true;
+         int freq[26]={0};
+         int n=s.size();
+
+         if(s.size() != t.size()) return false;
+
+         for(int i=0;i<s.size();i++){
+            freq[s[i]-'a']++;
+            freq[t[i]-'a']--;
+         }
+         for(auto it : freq){
+            if(it!=0) return false;
+         }
+         return true;
+
+
+        // map<char,int> mpp;
+        // if(s.size() != t.size()){
+        //     return false;
+        // }
+
+        // for(int i=0;i<s.size();i++){
+        //     mpp[s[i]]++;
+        //     mpp[t[i]]--;
+        // }
+        // for(auto it : mpp){
+        //     if(it.second !=0){
+        //         return false;
+        //     }
+        // }
+        // return true;
 
         
     }
