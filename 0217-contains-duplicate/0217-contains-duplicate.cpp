099afb2f5489f4ccdc9e2911f4287896ecbcbfbc
Srey Patel <83701089+Srey-3112@users.noreply.github.com>
2025-02-20 23:32:37 +0530

    Time: 37 ms (34.14%), Space: 73.6 MB (35.98%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        // map<int,int> mpp;
+        // for(int i=0;i<nums.size();i++){
+        //     mpp[nums[i]]++;
+        // }
+        // for(auto it: mpp){
+        //     if(it.second >= 2){
+        //         return true;
+        //     }
+        // }
+        // return false;
+
+
+        // sort(nums.begin(), nums.end());
+        // for(int i=1;i<nums.size();i++){
+        //     if(nums[i] == nums[i-1]){
+        //         return true;
+        //     }
+        // }
+        // return false;
+
+        unordered_set<int> seen;
+        for(auto num: nums){
+            if(seen.count(num)) return true;
+            seen.insert(num);
+        }
+        return false;
+    }
+};
