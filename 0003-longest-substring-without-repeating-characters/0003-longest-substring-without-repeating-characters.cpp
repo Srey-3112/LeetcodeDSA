cc4c34b6c21add43cbe393dd7b242d715bf8d1bc
Srey Patel <83701089+Srey-3112@users.noreply.github.com>
2025-03-01 04:35:53 +0530

    Time: 19 ms (31.22%), Space: 14.4 MB (15.23%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -2,20 +2,60 @@ class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
 
-        int len=0;
+        // int len=0;
+        // int maxLen=0;
+        // int n= s.size();
+
+        // for(int i=0;i<n;i++){
+        //     int hash[255]={0};
+        //     for(int j=i;j<n;j++){
+        //         if(hash[s[j]] == 1) break;
+
+        //         len=j-i+1;
+        //         maxLen=max(len,maxLen);
+        //         hash[s[j]]++;
+        //     }
+        // }
+
+        // int maxLen=0;
+        // int n=s.size();
+
+        // int hash[255]={-1};
+        // int left=0;
+        // int right=0;
+
+        // if(!s.empty()){
+        //     while(right<n){
+
+        //         if(hash[s[right]] > -1){
+        //             if(hash[s[right]] >= left){
+        //                 left= hash[s[right]]+1;
+        //             } 
+        //         }
+                
+        //         hash[s[right]]=right;
+        //         int len= right-left+1;
+        //         maxLen=max(len,maxLen);
+        //         right++;
+        //     }
+        // }
+
+
+        unordered_set<char> charSet;
+        int l=0;
         int maxLen=0;
-        int n= s.size();
 
-        for(int i=0;i<n;i++){
-            int hash[255]={0};
-            for(int j=i;j<n;j++){
-                if(hash[s[j]] == 1) break;
+        for(int r=0;r<s.size();r++){
 
-                len=j-i+1;
-                maxLen=max(len,maxLen);
-                hash[s[j]]++;
+            while(charSet.find(s[r]) != charSet.end()){
+                charSet.erase(s[l]);
+                l++;
             }
+
+            maxLen=max(maxLen,r-l+1);
+            charSet.insert(s[r]);
         }
+
 
         return maxLen;
 
