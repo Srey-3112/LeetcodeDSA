16f2f2535898f284a04bd1257734da9f8b6dc9bb
Srey Patel <83701089+Srey-3112@users.noreply.github.com>
2025-03-11 04:04:09 +0530

    Time: 4 ms (65.58%), Space: 10.9 MB (73.07%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -2,23 +2,44 @@ class Solution {
 public:
     int characterReplacement(string s, int k) {
 
+        
+
+        // int maxLen=0;
+        // for(int i=0;i<s.size();i++){
+        //     int hash[26]={0};
+        //     int maxF=0;
+        //     for(int j=i;j<s.size();j++){
+        //         hash[s[j]-'A']++;
+        //         maxF=max(maxF, hash[s[j]-'A']);
+        //         int changes= (j-i+1)-maxF;
+        //         if(changes<=k){
+        //             maxLen=max(maxLen,(j-i+1));
+        //         }
+        //         else{
+        //             break;
+        //         }
+        //     }
+        // }
+        // return maxLen;
+
         int maxLen=0;
-        int n=s.size();
-        for(int i=0;i<n;i++){
-            int hash[26]={0};
-            int maxF=0;
-            for(int j=i;j<n;j++){
-                hash[s[j] - 'A']++;
-                maxF=max(maxF,hash[s[j] - 'A']);
-                int rep=(j-i+1)-maxF;
-                if(rep<=k){
-                    maxLen=max(maxLen, (j-i+1));
-                }else{
-                    break;
-                }
+        int l=0, r=0;
+        int maxF=0;
+        map<char,int> mpp;
+
+        while(r<s.size()){
+            mpp[s[r]]++;
+            maxF=max(maxF,mpp[s[r]]);
+            int changes= (r-l+1)-maxF;
+            if(changes <= k){
+                maxLen=max(maxLen, r-l+1);
             }
+            else{
+                mpp[s[l]]--;
+                l++;
+            }
+            r++;
         }
         return maxLen;
-        
     }
 };
