d420e5956c03d27370cd4a6df2abc21aa245ec29
Srey Patel <83701089+Srey-3112@users.noreply.github.com>
2025-03-13 21:29:53 +0530

    Time: 3 ms (66.54%), Space: 10 MB (44.96%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,41 +1,53 @@
 class Solution {
 public:
-    bool isFreqSame(int freq1[],int freq2[]){
-
-        for(int i=0;i<26;i++){
-            if(freq1[i]!=freq2[i]){
-                return false;
-            }
-        }
-        return true;
-    }
-
-
     bool checkInclusion(string s1, string s2) {
-        int freq[26] ={0};
+        // int freq[26] ={0};
+
+        // for(int i=0;i<s1.size();i++){
+        //     freq[s1[i]-'a']++;
+        // }
+
+        
+
+        // for(int i=0;i<s2.size();i++){
+        //     int windIdx= 0, idx=i;
+        //     int windFreq[26]={0};
+
+        //     while(windIdx<s1.size() && idx<s2.size()){
+        //         windFreq[s2[idx]-'a']++;
+        //         idx++;
+        //         windIdx++;
+        //     }
+
+        //     if(isFreqSame(freq,windFreq)){
+        //         return true;
+        //     }
+        // }
+
+        // return false;
+
+        vector<int> freqs1(26,0);
+        vector<int> freqs2(26,0);
 
         for(int i=0;i<s1.size();i++){
-            freq[s1[i]-'a']++;
+            freqs1[s1[i]-'a']++;
         }
 
+        int i=0,j=0;
+        while(j<s2.size()){
+
+            if((j-i+1)<=s1.size()){
+                freqs2[s2[j]-'a']++;
+                j++;
+            }
+            else{
+                freqs2[s2[i]-'a']--;
+                i++;
+            }
+
+            if(freqs1 ==freqs2) return true;
+        }
         
-
-        for(int i=0;i<s2.size();i++){
-            int windIdx= 0, idx=i;
-            int windFreq[26]={0};
-
-            while(windIdx<s1.size() && idx<s2.size()){
-                windFreq[s2[idx]-'a']++;
-                idx++;
-                windIdx++;
-            }
-
-            if(isFreqSame(freq,windFreq)){
-                return true;
-            }
-        }
-
         return false;
-        
     }
 };
