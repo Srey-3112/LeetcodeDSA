c42774fda6d57d82bbb9506c3d9c8554ea60f774
Srey Patel <83701089+Srey-3112@users.noreply.github.com>
2025-05-03 01:11:30 +0530

    Time: 15 ms (45.97%), Space: 18.9 MB (91.37%) - LeetHub

--- 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.cpp
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.cpp
@@ -19,19 +19,22 @@ public:
 class Solution {
 public:
     Node* connect(Node* root) {
-      if (!root || !root->left) 
-            return root;
+
+        Node* levelhead=root;
+         while(levelhead && levelhead->left){
+
+            Node* cur=levelhead;
+            while(cur){
+
+                cur->left->next=cur->right;
+                if(cur->next){
+                    cur->right->next=cur->next->left;
+                }
+                cur=cur->next;
+            }
+            levelhead=levelhead->left;
+         }
+         return root;
         
-        
-        root->left->next = root->right;
-        
-        
-        if (root->next) 
-            root->right->next = root->next->left;
-        
-        connect(root->left);
-        connect(root->right);
-        
-        return root;  
     }
 };
