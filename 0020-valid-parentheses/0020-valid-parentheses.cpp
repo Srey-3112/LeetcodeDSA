409013e995b5e5c8b037f11c01e1dd62235a66d8
Srey Patel <83701089+Srey-3112@users.noreply.github.com>
2025-05-06 02:06:43 +0530

    Time: 0 ms (100%), Space: 8.6 MB (83.55%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,42 +1,25 @@
 class Solution {
 public:
-    int typeOfBracket(char bracket) {
-        if (bracket == '(' || bracket == ')') // first type of bracket 
-            return 1;
-        else if (bracket == '{' || bracket == '}')// second type of bracket 
-            return 2;
-        else
-            return 3; // third type of bracket 
-
-        return -1;
-    }
-
     bool isValid(string s) {
-        string openBrackets = "";
 
-        for (int i = 0; i < s.size(); i++) {
-            // if we found open bracket store them in openBrackets
-            if (s[i] == '(' || s[i] == '{' || s[i] == '[') {
-                openBrackets += s[i];
-            } else {
-                // if we found a close bracket , we will check if the type of
-                // this bracket and last open bracket is same
+        stack<char> st;
+        for(int i=0;i<s.size();i++){
 
-                if (openBrackets.size() == 0)
-                    return false; // edge case if we found close bracket when do
-                                  // don't have any open bracket return false
-                if (typeOfBracket(s[i]) !=
-                    typeOfBracket(openBrackets.at(openBrackets.size() - 1)))
+            if(s[i]=='(' || s[i]=='[' || s[i]=='{'){
+                st.push(s[i]);
+            }else{
+                if(st.empty()) return false;
+                char ob=st.top();
+                st.pop();
+                if((s[i]==')' && ob=='(') || (s[i]==']' && ob=='[') || (s[i]=='}' && ob=='{')){
+                    continue;
+                }else{
                     return false;
-                else
-                    openBrackets.pop_back();
+                }
             }
+
         }
-
-        // if we still have any brackets open return false
-        if (openBrackets.size() != 0)
-            return false;
-
+        if(!st.empty()) return false;
         return true;
     }
 };
