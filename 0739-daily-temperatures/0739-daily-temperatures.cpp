3f383045707ccd789338e46ffaca4f964dddcb54
Srey Patel <83701089+Srey-3112@users.noreply.github.com>
2025-05-07 00:21:13 +0530

    Time: 12 ms (91.77%), Space: 102.6 MB (98.57%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -1,29 +1,55 @@
 #include <vector>
 using namespace std;
 
+//TC: O(2N) = O(n)
+
+
 class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& temperatures) {
-        int n = temperatures.size();
-        stack<int> st;
-        vector<int> result(n);
+    //     int n = temperatures.size();
+    //     stack<int> st;
+    //     vector<int> result(n);
 
-        for(int i=n-1;i>=0;i--){
+    //     for(int i=n-1;i>=0;i--){
 
-            while(!st.empty() && temperatures[i]>=temperatures[st.top()]){
-                st.pop();
-            }
+    //         while(!st.empty() && temperatures[i]>=temperatures[st.top()]){
+    //             st.pop();
+    //         }
 
-            if(st.empty()){
-                result[i]=0;
-            }else{
-                result[i]=st.top()-i;
-            }
+    //         if(st.empty()){
+    //             result[i]=0;
+    //         }else{
+    //             result[i]=st.top()-i;
+    //         }
 
-            st.push(i);
+    //         st.push(i);
+    //     }
+
+    //     return result;
+    // }
+
+    // optimised without stack
+    int n = temperatures.size();
+    vector<int> result(n, 0);
+    int hottest = 0;
+    
+    for (int i = n - 1; i >= 0; --i) {
+        if(temperatures[i]>=hottest){
+            hottest=temperatures[i];
+            continue;
         }
 
-        return result;
+        int day=1;
+        while(temperatures[i]>=temperatures[i+day]){
+            day=day+ result[i+day];
+        }
+
+        result[i]=day;
+    }
+    return result;
+    
+     
     }
         
 };
