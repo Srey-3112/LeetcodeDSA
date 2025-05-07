9a9087c78454a8c787b808e81c7e1fdf848710df
Srey Patel <83701089+Srey-3112@users.noreply.github.com>
2025-05-07 09:21:52 +0530

    Time: 16 ms (91.23%), Space: 78.8 MB (9.55%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -1,54 +1,57 @@
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
-        priority_queue<int,vector<int>,greater<int>> pq;
+        //priority_queue<int,vector<int>,greater<int>> pq;
 
         // minheap 
-        for(auto it: nums){
-            pq.push(it);
-            if(pq.size()>k){
-                pq.pop();
-            }
+        // TC O(nlogk)
+        // for(auto it: nums){
+        //     pq.push(it);
+        //     if(pq.size()>k){
+        //         pq.pop();
+        //     }
 
-        }
+        // }
         //max Heap
-
+        //TC O(nlogn)
         // for(auto it:nums){
         //     pq.push(it);
         // }
         // for(int i=0;i<k-1;i++){
         //     pq.pop();
         // }
-        return pq.top();
+        //return pq.top();
+        return quickSelect(nums,k);
     }
 
-    // int quickSelect(vector<int> &nums,int k){
-    //     // Select pivot at random.
-    //     int pivot = nums[rand() % nums.size()];
+    int quickSelect(vector<int> &nums,int k){
+        // Select pivot at random.
+        int pivot = nums[rand() % nums.size()];
 
-    //     vector<int> left;
-    //     vector<int> mid;
-    //     vector<int> right;
+        vector<int> left;
+        vector<int> mid;
+        vector<int> right;
 
-    //     for(int num:nums){
-    //         if(num > pivot){
-    //             left.push_back(num);
-    //         }
-    //         else if(num<pivot){
-    //             right.push_back(num);
-    //         }
-    //         else{
-    //             mid.push_back(num);
-    //         }
-    //     }
+        for(int num:nums){
+            if(num > pivot){
+                left.push_back(num);
+            }
+            else if(num<pivot){
+                right.push_back(num);
+            }
+            else{
+                mid.push_back(num);
+            }
+        }
 
-    //     if(k <= left.size()){
-    //         return quickSelect(left,k);
-    //     }
+        if(k <= left.size()){
+            return quickSelect(left,k);
+        }
 
-    //     if(left.size() + mid.size() < k){
-    //         return quickSelect(right, k - left.size() - mid.size());
-    //     }
-    //     return pivot;
+        if(left.size() + mid.size() < k){
+            return quickSelect(right, k - left.size() - mid.size());
+        }
+        return pivot;
+    }
     
 };
