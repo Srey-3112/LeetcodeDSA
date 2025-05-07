50c36a32e4ddd5e117efcdb00794080c3441b2c4
Srey Patel <83701089+Srey-3112@users.noreply.github.com>
2025-05-07 08:39:16 +0530

    Time: 0 ms (100%), Space: 38.2 MB (83.08%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.cpp
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -2,47 +2,66 @@ class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
 
+        //Brute force using PQ.
+
+        // vector<int> freq(26,0);
+        // for(char &ch:tasks){
+        //     freq[ch-'A']++;
+        // }
+
+        // priority_queue<int> pq;
+        // int time=0;
+        
+        // for(int i=0;i<26;i++){
+        //     if(freq[i]>0){
+        //         pq.push(freq[i]);
+        //     }
+        // }
+
+        
+        // while(!pq.empty()){
+
+        //     vector<int> temp;
+        //     for(int i=1;i<=n+1;i++){
+        //         if(!pq.empty()){
+        //             int f=pq.top();
+        //             pq.pop();
+        //             f--;
+        //             temp.push_back(f);
+        //         }
+        //     }
+
+        //     for(int &it:temp){
+        //         if(it>0){
+        //             pq.push(it);
+        //         }
+        //     }
+
+        //     if(pq.empty()){
+        //         time+= temp.size();
+        //     }else{
+        //         time+= n+1;
+        //     }
+        // }
+
+        // return time;
+
+        //optimised
+
         vector<int> freq(26,0);
         for(char &ch:tasks){
             freq[ch-'A']++;
         }
 
-        priority_queue<int> pq;
-        int time=0;
-        
-        for(int i=0;i<26;i++){
-            if(freq[i]>0){
-                pq.push(freq[i]);
-            }
+        sort(freq.begin(),freq.end());
+        int maxFreq=freq[25]-1;
+        int idleSlot= maxFreq*n;
+
+        for(int i=24;i>=0 && freq[i]>0;i--){
+            idleSlot-= min(maxFreq,freq[i]);
         }
 
-        
-        while(!pq.empty()){
-
-            vector<int> temp;
-            for(int i=1;i<=n+1;i++){
-                if(!pq.empty()){
-                    int f=pq.top();
-                    pq.pop();
-                    f--;
-                    temp.push_back(f);
-                }
-            }
-
-            for(int &it:temp){
-                if(it>0){
-                    pq.push(it);
-                }
-            }
-
-            if(pq.empty()){
-                time+= temp.size();
-            }else{
-                time+= n+1;
-            }
-        }
-
-        return time;
+        return idleSlot>0 ? idleSlot+tasks.size() : tasks.size();
         
     }
 };
