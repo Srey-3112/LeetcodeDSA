b25b92919ae90a1ba8a99035b9afba23728459d7
Srey Patel <83701089+Srey-3112@users.noreply.github.com>
2025-05-07 08:54:11 +0530

    Time: 32 ms (55.65%), Space: 64.5 MB (21.95%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -1,36 +1,43 @@
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
-        return quickSelect(nums,k);
+        priority_queue<int> pq;
+        for(auto it:nums){
+            pq.push(it);
+        }
+        for(int i=0;i<k-1;i++){
+            pq.pop();
+        }
+        return pq.top();
     }
 
-    int quickSelect(vector<int> &nums,int k){
-        // Select pivot at random.
-        int pivot = nums[rand() % nums.size()];
+    // int quickSelect(vector<int> &nums,int k){
+    //     // Select pivot at random.
+    //     int pivot = nums[rand() % nums.size()];
 
-        vector<int> left;
-        vector<int> mid;
-        vector<int> right;
+    //     vector<int> left;
+    //     vector<int> mid;
+    //     vector<int> right;
 
-        for(int num:nums){
-            if(num > pivot){
-                left.push_back(num);
-            }
-            else if(num<pivot){
-                right.push_back(num);
-            }
-            else{
-                mid.push_back(num);
-            }
-        }
+    //     for(int num:nums){
+    //         if(num > pivot){
+    //             left.push_back(num);
+    //         }
+    //         else if(num<pivot){
+    //             right.push_back(num);
+    //         }
+    //         else{
+    //             mid.push_back(num);
+    //         }
+    //     }
 
-        if(k <= left.size()){
-            return quickSelect(left,k);
-        }
+    //     if(k <= left.size()){
+    //         return quickSelect(left,k);
+    //     }
 
-        if(left.size() + mid.size() < k){
-            return quickSelect(right, k - left.size() - mid.size());
-        }
-        return pivot;
-    }
+    //     if(left.size() + mid.size() < k){
+    //         return quickSelect(right, k - left.size() - mid.size());
+    //     }
+    //     return pivot;
+    
 };
