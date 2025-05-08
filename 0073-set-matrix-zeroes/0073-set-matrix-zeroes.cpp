b269edf92383b2a05df3f4bfc0a7064d02457b4d
Srey Patel <83701089+Srey-3112@users.noreply.github.com>
2025-05-09 01:09:22 +0530

    Time: 0 ms (100%), Space: 18.6 MB (52.73%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -2,27 +2,62 @@ class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
 
-        int n=matrix.size();
-        int m=matrix[0].size();
+        // TC: O(m*n)
+        // SC: O(n)+O(m)
 
-        vector<int> row(n,0);
-        vector<int> col(m,0);
+        //int n=matrix.size();
+        //int m=matrix[0].size();
 
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(matrix[i][j]==0){
-                    row[i]=1;
-                    col[j]=1;
+        // vector<int> row(n,0);
+        // vector<int> col(m,0);
+
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<m;j++){
+        //         if(matrix[i][j]==0){
+        //             row[i]=1;
+        //             col[j]=1;
+        //         } 
+        //     }
+        // }
+
+        
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<m;j++){
+        //         if(row[i]==1 || col[j ]==1){
+        //             matrix[i][j]=0;
+        //         }
+        //     }
+        // }
+
+        bool isCol = false;
+        int R = matrix.size();
+        int C = matrix[0].size();
+        for (int i = 0; i < R; i++) {
+            if (matrix[i][0] == 0) {
+                isCol = true;
+            }
+            for (int j = 1; j < C; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[0][j] = 0;
+                    matrix[i][0] = 0;
                 }
             }
         }
-
-        
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(row[i]==1 || col[j ]==1){
-                    matrix[i][j]=0;
+        for (int i = 1; i < R; i++) {
+            for (int j = 1; j < C; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
                 }
+            }
+        }
+        if (matrix[0][0] == 0) {
+            for (int j = 0; j < C; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+        if (isCol) {
+            for (int i = 0; i < R; i++) {
+                matrix[i][0] = 0;
             }
         }
         
