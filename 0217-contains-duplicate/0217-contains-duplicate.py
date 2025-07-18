c83b7b7ebf7adf1aaba2ea0feef8b224c84f546c
Srey Patel <83701089+Srey-3112@users.noreply.github.com>
2025-07-18 18:36:04 +0530

    Time: 7 ms (97.26%), Space: 25.9 MB (68.9%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -0,0 +1,44 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        
+        ### brute force
+        #TC O(n2)
+        #SC O(1)
+
+        # for i in range(len(nums)):
+        #     for j in range(i+1,len(nums)):
+        #         if nums[i]==nums[j]:
+        #             return True
+
+        
+        # return False
+
+        ###sorting
+        ## TC O(nlogn)
+        ## SC O(1) 
+
+        # nums.sort()
+
+        # for i in range(1,len(nums)):
+        #     if nums[i]==nums[i-1]:
+        #         return True
+
+        # return False
+
+        ### HashSet
+
+        # seen=set()
+        # for num in nums:
+        #     if num in seen:
+        #         return True
+        #     seen.add(num)
+
+        # return False
+
+        ### HashSet Length
+
+        return len(set(nums)) < len(nums)
