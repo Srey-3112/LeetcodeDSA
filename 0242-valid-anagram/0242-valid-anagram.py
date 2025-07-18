2d1f6c26620abcafedf052c5434c6ae4e71a284b
Srey Patel <83701089+Srey-3112@users.noreply.github.com>
2025-07-18 19:16:27 +0530

    Time: 22 ms (58.64%), Space: 13.4 MB (41.27%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+
+        ### sorting
+
+        if len(s)!=len(t):
+            return False
+
+        return sorted(s)==sorted(t)
+        
