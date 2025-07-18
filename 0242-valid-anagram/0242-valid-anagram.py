c9960086b1a3f6158bb27c67a54c269ebf3becea
Srey Patel <83701089+Srey-3112@users.noreply.github.com>
2025-07-18 19:50:15 +0530

    Time: 22 ms (58.64%), Space: 14.3 MB (15.46%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -8,8 +8,44 @@ class Solution(object):
 
         ### sorting
 
+        # if len(s)!=len(t):
+        #     return False
+
+        # return sorted(s)==sorted(t)
+
+
+        ### hashtable
+        ## SC O(1) since at max 26 character
+        ## TC O(n+m)
+
+
+        # if len(s)!=len(t):
+        #     return False
+
+        # tableS,tableT={},{}
+
+        # for i in range(len(s)):
+        #     tableS[s[i]] = 1+tableS.get(s[i],0)
+        #     tableT[t[i]] = 1+tableT.get(t[i],0)
+
+        # return tableS==tableT
+
+        ### hasthtable using array
+
+        arr= [0]*26
         if len(s)!=len(t):
             return False
 
-        return sorted(s)==sorted(t)
+        for i in range(len(s)):
+            arr[ord(s[i]) - ord('a')]+=1
+            arr[ord(t[i]) - ord('a')]-=1
+
+        for val in arr:
+            if val!=0:
+                return False
+
+        return True
+
+
+
         
