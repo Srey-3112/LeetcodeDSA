f37bcd2fd13a1e00fe63846b1f4a2430a8843d85
Srey Patel <83701089+Srey-3112@users.noreply.github.com>
2025-07-23 13:47:09 -0400

    Time: 78 ms (5.42%), Space: 12.5 MB (37.43%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.py
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -1,38 +1,77 @@
 class Solution(object):
 
-    def func(self,arr,ind,sum,current,result):
+    # def func(self,arr,ind,sum,current,result):
 
-        if sum==0:
-            result.append(current[:])
-            return 
+    #     if sum==0:
+    #         result.append(current[:])
+    #         return 
 
-        if sum<0 or ind==len(arr):
-            return
-
-
-        current.append(arr[ind])
-        self.func(arr,ind+1,sum-arr[ind],current,result)
-        current.pop()
-
-        for i in range(ind+1,len(arr)):
-            if arr[i] != arr[ind]:
-                self.func(arr,i,sum,current,result)
-                break
+    #     if sum<0 or ind==len(arr):
+    #         return
 
 
-    def combinationSum2(self, candidates, target):
-        """
-        :type candidates: List[int]
-        :type target: int
-        :rtype: List[List[int]]
-        """
+    #     current.append(arr[ind])
+    #     self.func(arr,ind+1,sum-arr[ind],current,result)
+    #     current.pop()
 
-        candidates.sort()
+    #     for i in range(ind+1,len(arr)):
+    #         if arr[i] != arr[ind]:
+    #             self.func(arr,i,sum,current,result)
+    #             break
 
-        result=[]
-        current=[]
 
-        self.func(candidates, 0, target, current, result)
+    # def combinationSum2(self, candidates, target):
+    #     """
+    #     :type candidates: List[int]
+    #     :type target: int
+    #     :rtype: List[List[int]]
+    #     """
 
-        return result
+    #     candidates.sort()
+
+    #     result=[]
+    #     current=[]
+
+    #     self.func(candidates, 0, target, current, result)
+
+    #     return result
+    
+        def __init__(self):
+            self.ans = []
+        
+        def func(self, ind, sum, nums, candidates):
+            # If the sum is zero, add the current combination to the result
+            if sum == 0:
+                self.ans.append(nums[:])
+                return
+            
+            # If the sum is negative or we have exhausted the candidates, return
+            if sum < 0 or ind == len(candidates):
+                return
+            
+            # Include the current candidate
+            nums.append(candidates[ind])
+            
+            # Recursively call with updated sum and next index
+            self.func(ind + 1, sum - candidates[ind], nums, candidates)
+            
+            # Backtrack by removing the last added candidate
+            nums.pop()
+            
+            # Skip duplicates: if not picking the current candidate, 
+            # ensure the next candidate is different
+            for i in range(ind + 1, len(candidates)):
+                if candidates[i] != candidates[ind]:
+                    self.func(i, sum, nums, candidates)
+                    break
+
+        def combinationSum2(self, candidates, target):
+            candidates.sort()  # Sort candidates to handle duplicates
+            self.ans = []
+            self.func(0, target, [], candidates)
+            return self.ans
+
+
+
+
         
