feca129c78acd8aa1cc31e5b9b64df6842bfef23
Srey Patel <83701089+Srey-3112@users.noreply.github.com>
2025-07-23 13:53:00 -0400

    Time: 3 ms (91.42%), Space: 17.9 MB (55.63%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.py
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -1,75 +1,22 @@
 class Solution:
-    def combinationSum2(self, nums, target):
-        self.res = []
-        self.count = defaultdict(int)
-        cur = []
-        A = []
-        
-        for num in nums:
-            if self.count[num] == 0:
-                A.append(num)
-            self.count[num] += 1
-        self.backtrack(A, target, cur, 0)
-        return self.res
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        res = []
+        candidates.sort()
 
-    def backtrack(self, nums, target, cur, i):
-        if target == 0:
-            self.res.append(cur[:])
-            return
-        if target < 0 or i >= len(nums):
-            return
-        
-        if self.count[nums[i]] > 0:
-            cur.append(nums[i])
-            self.count[nums[i]] -= 1
-            self.backtrack(nums, target - nums[i], cur, i)
-            self.count[nums[i]] += 1
-            cur.pop()
+        def dfs(idx, path, cur):
+            if cur == target:
+                res.append(path.copy())
+                return
+            for i in range(idx, len(candidates)):
+                if i > idx and candidates[i] == candidates[i - 1]:
+                    continue
+                if cur + candidates[i] > target:
+                    break
 
-        self.backtrack(nums, target, cur, i + 1)
+                path.append(candidates[i])
+                dfs(i + 1, path, cur + candidates[i])
+                path.pop()
 
-
-#class Solution(object):
-
-    # def func(self,arr,ind,sum,current,result):
-
-    #     if sum==0:
-    #         result.append(current[:])
-    #         return 
-
-    #     if sum<0 or ind==len(arr):
-    #         return
-
-
-    #     current.append(arr[ind])
-    #     self.func(arr,ind+1,sum-arr[ind],current,result)
-    #     current.pop()
-
-    #     for i in range(ind+1,len(arr)):
-    #         if arr[i] != arr[ind]:
-    #             self.func(arr,i,sum,current,result)
-    #             break
-
-
-    # def combinationSum2(self, candidates, target):
-    #     """
-    #     :type candidates: List[int]
-    #     :type target: int
-    #     :rtype: List[List[int]]
-    #     """
-
-    #     candidates.sort()
-
-    #     result=[]
-    #     current=[]
-
-    #     self.func(candidates, 0, target, current, result)
-
-    #     return result
-    
-       
-
-
-
-
+        dfs(0, [], 0)
+        return res
         
