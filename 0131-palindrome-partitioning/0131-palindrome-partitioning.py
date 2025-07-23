c8b829f7030aa2a2a9ab547d3a97864342b1bcef
Srey Patel <83701089+Srey-3112@users.noreply.github.com>
2025-07-24 02:56:03 +0530

    Time: 92 ms (20.95%), Space: 47.9 MB (66.29%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.py
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -2,26 +2,29 @@ class Solution:
     def partition(self, s):
         #your code goes here
         ans=[]
-        self.func(0,s,[],ans)
+        current=[]
+        
+        
+        def palindrome(s,start, end):            
+            while start <= end:
+                if s[start] != s[end]:
+                    return False
+                start += 1
+                end -= 1
+            return True
+
+        def func(ind):
+
+            if ind==len(s):
+                ans.append(current[:])
+                return
+
+            for i in range(ind,len(s)):
+                if palindrome(s,ind,i):
+                    subStr=s[ind:i+1]
+                    current.append(subStr)
+                    func(i+1)
+                    current.pop()
+        
+        func(0)
         return ans
-
-    def palindrome(self,s,start, end):            
-        while start <= end:
-            if s[start] != s[end]:
-                return False
-            start += 1
-            end -= 1
-        return True
-
-    def func(self,ind,s,current,ans):
-
-        if ind==len(s):
-            ans.append(current[:])
-            return
-
-        for i in range(ind,len(s)):
-            if self.palindrome(s,ind,i):
-                subStr=s[ind:i+1]
-                current.append(subStr)
-                self.func(i+1,s,current,ans)
-                current.pop()
