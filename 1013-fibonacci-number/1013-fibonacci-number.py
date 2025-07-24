db28586374830997ac8d2be0a1dca430142f1f7a
Srey Patel <83701089+Srey-3112@users.noreply.github.com>
2025-07-24 17:15:28 +0530

    Time: 3 ms (99.83%), Space: 12.3 MB (98.28%) - LeetHub

---
+++ 1013-fibonacci-number/1013-fibonacci-number.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def f(self,n,dp):
+        if n<=1:
+            return n
+
+        if dp[n] !=-1:
+            return dp[n]
+        
+        dp[n]=self.f(n-1,dp)+self.f(n-2,dp)
+        return dp[n]    
+
+    def fib(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        dp=[-1]*(n+1)
+        return self.f(n,dp)
+
+       
+
+        
