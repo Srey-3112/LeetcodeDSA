c85c50236ef1e96e2185d1d40d7715c2effe0b9a
Srey Patel <83701089+Srey-3112@users.noreply.github.com>
2025-07-24 21:44:52 +0530

    Time: 1339 ms (20.15%), Space: 52.9 MB (7.84%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.py
@@ -0,0 +1,23 @@
+class Solution:
+    def coinChange(self, coins, amount):
+
+        dp={}
+
+        def dfs(amount):
+            if amount == 0:
+                return 0
+            if amount in dp:
+                return dp[amount]
+            res = 1e9
+            for coin in coins:
+                if amount - coin >= 0:
+                    res = min(res, 1 + dfs(amount - coin))
+
+
+            dp[amount]=res
+            return res
+
+
+        minCoins = dfs(amount)
+
+        return -1 if minCoins >= 1e9 else minCoins
