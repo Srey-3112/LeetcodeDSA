2dc963c60d0766e1ba58a98be991b057ff569455
Srey Patel <83701089+Srey-3112@users.noreply.github.com>
2025-07-24 18:13:55 +0530

    Time: 0 ms (100%), Space: 12.6 MB (12.4%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,22 +1,30 @@
 class Solution(object):
 
-    def func(self,n,dp):
+    # def func(self,n,dp):
 
-        if n<=1:
-            return 1
+    #     if n<=1:
+    #         return 1
 
-        if dp[n]!=-1:
-            return dp[n]
+    #     if dp[n]!=-1:
+    #         return dp[n]
 
-        dp[n]=self.func(n-1,dp)+self.func(n-2,dp)
+    #     dp[n]=self.func(n-1,dp)+self.func(n-2,dp)
 
-        return dp[n]
+    #     return dp[n]
     
     def climbStairs(self, n):
         """
         :type n: int
         :rtype: int
         """
-        dp=[-1]*(n+1)
-        return self.func(n,dp)
+        prev=prev2=1
+        if n<=1:
+            return prev
+        
+        for i in range(2,n+1):
+            curr_i=prev+prev2
+            prev2=prev
+            prev=curr_i
+
+        return prev 
         
