d95a7fa9be4fe27d44a9984a01fe889503f17a34
Srey Patel <83701089+Srey-3112@users.noreply.github.com>
2025-07-24 17:33:58 +0530

    Time: 8 ms (95.76%), Space: 12.2 MB (98.28%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.py
+++ 1013-fibonacci-number/1013-fibonacci-number.py
@@ -1,23 +1,33 @@
 # TC: O(n)
 # SC: O(n) stack space + O(n) dp array
 class Solution(object):
-    def f(self,n,dp):
-        if n<=1:
-            return n
+    # def f(self,n,dp):
+    #     if n<=1:
+    #         return n
 
-        if dp[n] !=-1:
-            return dp[n]
+    #     if dp[n] !=-1:
+    #         return dp[n]
         
-        dp[n]=self.f(n-1,dp)+self.f(n-2,dp)
-        return dp[n]    
+    #     dp[n]=self.f(n-1,dp)+self.f(n-2,dp)
+    #     return dp[n]    
 
     def fib(self, n):
         """
         :type n: int
         :rtype: int
         """
-        dp=[-1]*(n+1)
-        return self.f(n,dp)
+        prev=1
+        prev2=0
+
+        if n==0:
+            return prev2
+            
+        for i in range(2,n+1):
+            cur_i=prev+prev2
+            prev2=prev
+            prev=cur_i
+
+        return prev
 
        
 
