cda09720caa64133dc4a5fff03f0ccdb604703da
Srey Patel <83701089+Srey-3112@users.noreply.github.com>
2025-07-24 20:21:07 +0530

    Time: 3 ms (71.93%), Space: 12.6 MB (26.33%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.py
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.py
@@ -4,21 +4,33 @@ class Solution(object):
         :type cost: List[int]
         :rtype: int
         """
-        dp=[-1]*len(cost)
-        def func(i):
+        
+        # def func(i):
 
-            if i>=len(cost):
-                return 0
+        #     if i>=len(cost):
+        #         return 0
             
-            if dp[i]!=-1:
-                return dp[i]
+        #     if dp[i]!=-1:
+        #         return dp[i]
             
-            dp[i]=cost[i]+min(func(i+1),func(i+2))
+        #     dp[i]=cost[i]+min(func(i+1),func(i+2))
 
-            return dp[i]
+        #     return dp[i]
+
+        n = len(cost)
+        dp = [0] * (n + 1)
+        
+        for i in range(2, n + 1):
+            dp[i] = min(dp[i - 1] + cost[i - 1],
+                        dp[i - 2] + cost[i - 2])
+        
+        return dp[n]
 
 
 
-        return min(func(0),func(1))
+        # for i in range(len(cost) - 3, -1, -1):
+        #     cost[i] += min(cost[i + 1], cost[i + 2])
+
+        # return min(cost[0], cost[1])
 
         
