d147d109f65af6d51e4bef5e26be05ea2b70976f
Srey Patel <83701089+Srey-3112@users.noreply.github.com>
2025-07-25 20:17:46 +0530

    Time: 7 ms (78.14%), Space: 17.7 MB (76.23%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -12,12 +12,17 @@ class Solution(object):
         #         count+=1
         #     res.append(count)
 
-        dp=[0]*(n+1)
-        offset=1
-        for i in range(1,n+1):
-            if offset*2==i:
-                offset=i
+        # dp=[0]*(n+1)
+        # offset=1
+        # for i in range(1,n+1):
+        #     if offset*2==i:
+        #         offset=i
             
-            dp[i]=1+dp[i-offset]
+        #     dp[i]=1+dp[i-offset]
+        # return dp
+
+        dp=[0]*(n+1)
+        for i in range(1,n+1):
+            dp[i]=dp[i>>1]+(i&1)
         return dp
         
