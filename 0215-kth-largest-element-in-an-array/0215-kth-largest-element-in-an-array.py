4058ce2bbece932b9bfa51fd67bd4bc345820e11
Srey Patel <83701089+Srey-3112@users.noreply.github.com>
2025-08-02 07:36:51 +0530

    Time: 301 ms (60.45%), Space: 20 MB (26.72%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -1,29 +1,46 @@
-import random
-
 class Solution:
+     
     def findKthLargest(self, nums, k):
-        return self.quickSelect(nums, k)
+        
+        
+        import heapq
+        pq=[]
+        for i in range(k):
+            heapq.heappush(pq, nums[i]) 
+        for i in range(k, len(nums)):  
+            if nums[i] > pq[0]:
+                heapq.heappop(pq)  
+                heapq.heappush(pq, nums[i])
+        return pq[0]   
 
-    def quickSelect(self, nums, k):
-        pivot = random.choice(nums)
 
-        left = []
-        mid = []
-        right = []
 
-        for num in nums:
-            if num > pivot:
-                left.append(num)
-            elif num < pivot:
-                right.append(num)
-            else:
-                mid.append(num)
+# import random
 
-        if k <= len(left):
-            return self.quickSelect(left, k)
-        if k > len(left) + len(mid):
-            return self.quickSelect(right, k - len(left) - len(mid))
-        return pivot
+# class Solution:
+#     def findKthLargest(self, nums, k):
+#         return self.quickSelect(nums, k)
+
+#     def quickSelect(self, nums, k):
+#         pivot = random.choice(nums)
+
+#         left = []
+#         mid = []
+#         right = []
+
+#         for num in nums:
+#             if num > pivot:
+#                 left.append(num)
+#             elif num < pivot:
+#                 right.append(num)
+#             else:
+#                 mid.append(num)
+
+#         if k <= len(left):
+#             return self.quickSelect(left, k)
+#         if k > len(left) + len(mid):
+#             return self.quickSelect(right, k - len(left) - len(mid))
+#         return pivot
 
         
         
